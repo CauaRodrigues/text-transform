f5b9352510e28f75ed0c6b60d9e5a9233d4df06d
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-08-01 18:04:13 -0300

    feat(user): indicador de conteúdo abaixo

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Header from "@/components/Header";
+import ScrollIndicatorIcon from "@/assets/icons/scroll-indicator.svg";
 import "./page.scss";
 
 export default function About() {
@@ -9,23 +10,51 @@ export default function About() {
 
       <main>
         <section className="container--hero_about">
-          <div className="box--content_text">
-            <h1>Sobre Nós</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipiscing elit dictumst
-              mollis, taciti fermentum posuere venenatis aliquet velit purus
-              malesuada penatibus, maecenas potenti condimentum sociosqu tempus
-              aptent magna primis, id felis enim tristique aliquam suspendisse
-              vulputate curae.
-            </p>
+          <div className="container--content_about">
+            <div className="box--content_text">
+              <h1>Sobre Nós</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit dictumst
+                mollis, taciti fermentum posuere venenatis aliquet velit purus
+                malesuada penatibus, maecenas potenti condimentum sociosqu
+                tempus aptent magna primis, id felis enim tristique aliquam
+                suspendisse vulputate curae.
+              </p>
+            </div>
+
+            <Image
+              src="/assets/images/hero-about.jpg"
+              alt="Pessoa digitando no notebook"
+              className="hero-about"
+              width={718}
+              height={478.67}
+            />
           </div>
 
           <Image
-            src="/assets/images/hero-about.jpg"
-            alt="Pessoa digitando no notebook"
-            width={718}
-            height={478.67}
+            src={ScrollIndicatorIcon}
+            alt="Scroll Indicator"
+            className="scroll_indicator"
+            width={55}
+            height={53}
           />
+        </section>
+
+        <section>
+          <div>
+            <h2>Motivação</h2>
+
+            <p>
+              Minha paixão por produtividade e criatividade inspirou a criação
+              desta plataforma. Ofereço um “Canivete Suíço” para facilitar a
+              criação e edição de textos curtos de forma rápida, dispensando
+              aplicações complexas que consomem seu tempo. Impulsionado pelo
+              desejo de tornar seu trabalho eficiente, desenvolvi ferramentas
+              poderosas e intuitivas. Seja você estudante, designer, criador de
+              conteúdo ou alguém que precise editar mensagens e e-mails, essa
+              ferramenta é perfeita para você.
+            </p>
+          </div>
         </section>
       </main>
     </>
