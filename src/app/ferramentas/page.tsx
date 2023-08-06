be7db5d3c87d76d22ab26f3a8067c37c7d28cb1a
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-08-05 23:41:53 -0300

    feat(user): conteúdo na anside

--- src/app/ferramentas/page.tsx
+++ src/app/ferramentas/page.tsx
@@ -11,7 +11,7 @@ export default function Tools() {
 
       <main className="page-tools">
         <aside>
-          <div className="field__search-tools">
+          <header className="field__search-tools">
             <input
               type="text"
               id="searchTool"
@@ -26,7 +26,59 @@ export default function Tools() {
                 height={27.02}
               />
             </button>
-          </div>
+          </header>
+
+          <section>
+            <div>
+              <h3>Manipular Texto</h3>
+              <ul>
+                <li>Formatação de Texto</li>
+                <li>Censurar Textos</li>
+                <li>Estilizar Texto</li>
+                <li>Ordenar Lista</li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Análise de Texto</h3>
+              <ul>
+                <li>Contador de Caracteres</li>
+                <li>Comparação de Textos</li>
+                <li>Localizar Palavras</li>
+                <li>Corretor Ortográfico</li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Aleatório</h3>
+              <ul>
+                <li>Sortear Números</li>
+                <li>Sortear Lista de Nomes</li>
+                <li>Sortear Equipes</li>
+                <li>Jogar Dados</li>
+                <li>Ordenação Aletória</li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Geradores</h3>
+              <ul>
+                <li>Gerar Lorem Ispum</li>
+                <li>Gerar Nomes</li>
+                <li>Gerar Senhas</li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Criptografia e Codificação</h3>
+              <ul>
+                <li>Código Morse</li>
+                <li>Código Binário</li>
+                <li>Código Hexadecimal</li>
+                <li>Criptografar Textos</li>
+              </ul>
+            </div>
+          </section>
         </aside>
 
         <article>
