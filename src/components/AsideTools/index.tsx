73693a4ddbe7779fa3fd07a851965667840afa9f
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-09-23 22:22:41 -0300

    feat(user): lista dinâmica dentro de anside bar

--- src/components/AsideTools/index.tsx
+++ src/components/AsideTools/index.tsx
@@ -1,19 +1,38 @@
 "use client";
 
-import SearchIcon from "@/assets/icons/utils/magnify-gray.svg";
-import FetchToolsService from "@/services/FetchTools";
+import { useState, useEffect, Fragment } from "react";
 import Image from "next/image";
 import Link from "next/link";
+
+import SearchIcon from "@/assets/icons/utils/magnify-gray.svg";
+import FetchToolsService from "@/services/FetchTools";
 
 import "./styles.scss";
 
 const srv = new FetchToolsService();
 
+interface ToolsListType {
+  id: number;
+  name: string;
+  list: {
+    id: number;
+    urlName: string;
+    title: string;
+    description: string;
+  }[];
+}
+
 export default function AsideTools() {
-  const sla = async () => {
-    const toolsName = await srv.listNamesAndCategories();
-    console.log(toolsName.data);
+  const [toolsList, setToolsList] = useState<ToolsListType[]>([]);
+
+  const loadTools = async () => {
+    const data = await srv.listNamesAndCategories();
+    setToolsList(data.tools.categories);
   };
+
+  useEffect(() => {
+    loadTools();
+  }, []);
 
   return (
     <aside className="menu__list--tools">
@@ -24,105 +43,32 @@ export default function AsideTools() {
           placeholder="Procurar"
           type="text"
         />
-        <button onClick={sla}>
+        <button>
           <Image alt="pesquisar" height={27.02} src={SearchIcon} width={27} />
         </button>
       </header>
 
       <section>
-        <div className="box__category--tools">
-          <h3>Manipular Texto</h3>
-          <ul>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Formatação de Texto</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Censurar Textos</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Estilizar Texto</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Ordenar Lista</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="box__category--tools">
-          <h3>Análise de Texto</h3>
-          <ul>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>
-                Contador de Caracteres
-              </Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Comparação de Textos</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Localizar Palavras</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Corretor Ortográfico</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="box__category--tools">
-          <h3>Aleatório</h3>
-          <ul>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Sortear Números</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>
-                Sortear Lista de Nomes
-              </Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Sortear Equipes</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Jogar Dados</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Ordenação Aletória</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="box__category--tools">
-          <h3>Geradores</h3>
-          <ul>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Gerar Lorem Ispum</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Gerar Nomes</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Gerar Senhas</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="box__category--tools">
-          <h3>Criptografia e Codificação</h3>
-          <ul>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Código Morse</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Código Binário</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Código Hexadecimal</Link>
-            </li>
-            <li>
-              <Link href={`/ferramentas/${"tool"}`}>Criptografar Textos</Link>
-            </li>
-          </ul>
-        </div>
+        {toolsList
+          ? toolsList.map((category) => {
+              return (
+                <Fragment key={category.id}>
+                  <div className="box__category--tools">
+                    <h3>{category.name}</h3>
+                    <ul>
+                      {category.list.map((tool) => (
+                        <li key={tool.id}>
+                          <Link href={`/ferramentas/${tool.urlName}`}>
+                            {tool.title}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </Fragment>
+              );
+            })
+          : null}
       </section>
     </aside>
   );
