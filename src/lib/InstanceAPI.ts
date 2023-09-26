36283b8b8264dbc9a7cf9c3a5cf5fa226007c62c
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-09-26 11:41:52 -0300

    fix(axios): url base da api guardada em variáveis de ambiente

--- src/lib/InstanceAPI.ts
+++ src/lib/InstanceAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const InstanceAPI = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
