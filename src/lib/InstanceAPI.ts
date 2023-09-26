a652498c7ef2b601e80b7528e1fe926ee7522352
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-09-26 12:30:23 -0300

    fix(api): variáveis de ambientes como públicas no lado do browser

--- src/lib/InstanceAPI.ts
+++ src/lib/InstanceAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const InstanceAPI = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
