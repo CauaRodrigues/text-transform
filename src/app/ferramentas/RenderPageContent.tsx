5d5f9a07cd685e1758a4ba8b71b10e5c1b40b2ab
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-09-29 23:20:08 -0300

    feat(user): animação de carregamento no menu lateral de ferramentas

--- src/app/ferramentas/RenderPageContent.tsx
+++ src/app/ferramentas/RenderPageContent.tsx
@@ -1,24 +1,43 @@
 "use client";
 
+import { Fragment, useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+
 import SectionTools from "@/components/SectionTools";
 import useDataStatus from "@/hooks/useDataStatus";
-import { Fragment } from "react";
 
 export default function RenderPageContent() {
+  const [loadingSkeleton, setLoadingSkeleton] = useState(true);
   const { content, isLoading } = useDataStatus();
+
+  useEffect(() => {
+    if (!isLoading) setLoadingSkeleton(false);
+  }, [isLoading]);
 
   return (
     <>
-      {isLoading
-        ? "Carregando"
-        : content.map((category) => (
-            <Fragment key={category.id}>
-              <SectionTools
-                titleSection={category.name}
-                data={category.tools}
-              />
-            </Fragment>
-          ))}
+      {loadingSkeleton ? (
+        <section className="skeleton__container">
+          <div className="skeleton-loading__title">
+            <Skeleton count={1} height={40} />
+          </div>
+
+          <div className="gallery__cards">
+            <Skeleton height={200} />
+            <Skeleton height={200} />
+            <Skeleton height={200} />
+            <Skeleton height={200} />
+            <Skeleton height={200} />
+            <Skeleton height={200} />
+          </div>
+        </section>
+      ) : (
+        content.map((category) => (
+          <Fragment key={category.id}>
+            <SectionTools titleSection={category.name} data={category.tools} />
+          </Fragment>
+        ))
+      )}
     </>
   );
 }
--- src/components/AsideTools/CategoriesAndTools.tsx
+++ src/components/AsideTools/CategoriesAndTools.tsx
@@ -4,31 +4,57 @@ import { Fragment } from "react";
 import Link from "next/link";
 import useDataStatus from "@/hooks/useDataStatus";
 
+import Skeleton from "react-loading-skeleton";
+
+const SkeletonLoadingAnimation = () => {
+  return (
+    <div
+      className="box__category--tools"
+      style={{ display: "flex", gap: "0.4rem", flexDirection: "column" }}
+    >
+      <Skeleton height={30} width={"80%"} />
+      <ul>
+        <Skeleton count={4} height={30} width={"85%"} />
+      </ul>
+
+      <Skeleton height={30} width={"80%"} />
+      <ul>
+        <Skeleton count={4} height={30} width={"85%"} />
+      </ul>
+
+      <Skeleton height={30} width={"80%"} />
+      <ul>
+        <Skeleton count={4} height={30} width={"85%"} />
+      </ul>
+    </div>
+  );
+};
+
 export default function CategoriesAndTools() {
   const { content, error, isLoading } = useDataStatus();
 
   return (
     <>
-      {isLoading
-        ? "Carregando..."
-        : error
-        ? "Opss... Um erro inesperado ocorreu."
-        : content.map((category) => (
-            <Fragment key={category.id}>
-              <div className="box__category--tools">
-                <h3>{category.name}</h3>
-                <ul>
-                  {category.tools.map((tool) => (
-                    <li key={tool.id}>
-                      <Link href={`/ferramentas/${tool.urlName}`}>
-                        {tool.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Fragment>
-          ))}
+      {isLoading ? (
+        <SkeletonLoadingAnimation />
+      ) : (
+        content.map((category) => (
+          <Fragment key={category.id}>
+            <div className="box__category--tools">
+              <h3>{category.name}</h3>
+              <ul>
+                {category.tools.map((tool) => (
+                  <li key={tool.id}>
+                    <Link href={`/ferramentas/${tool.urlName}`}>
+                      {tool.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Fragment>
+        ))
+      )}
     </>
   );
 }
